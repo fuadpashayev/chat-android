eebbab72a06ab78fa325f033d994f648121170f9
Fuad Pashayev <fuad.pashayev12@gmail.com>
2018-08-10 18:05:59 +0400

    ela chat

--- app/src/main/java/com/app/chat/MainActivity.kt
+++ app/src/main/java/com/app/chat/MainActivity.kt
@@ -2,34 +2,41 @@ package com.app.chat
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.TabLayout
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tabSign.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                when (tab.position) {
-                    0->{
-                        signIn.visibility = View.VISIBLE
-                        signUp.visibility = View.GONE
-                    }
-                    1->{
-                        signUp.visibility = View.VISIBLE
-                        signIn.visibility = View.GONE
-                    }
-                }
+        openSignIn.setOnClickListener {
+            signUpFrame.visibility = View.GONE
+            signInFrame.visibility = View.VISIBLE
+        }
+
+        openSignUp.setOnClickListener {
+            signInFrame.visibility = View.GONE
+            signUpFrame.visibility = View.VISIBLE
+        }
+
+
+
+
+
+        signIn.setOnClickListener {
+            val login = login_email.text
+            val pass = login_pass.text
+
+            if(login.count()>3 && pass.count()>3){
+                Log.d("------result","login: $login , pass: $pass")
+            }else{
+                Log.d("------result","ERRRRROOOORRRR")
             }
 
-            override fun onTabReselected(tab: TabLayout.Tab?){}
-            override fun onTabUnselected(tab: TabLayout.Tab?){}
-        })
+        }
 
     }
 }
